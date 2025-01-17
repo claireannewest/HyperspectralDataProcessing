f1848bad1caf78e3670f5ff37b8a88731092381c
Claire West <clairewest@eduroamprvnat-172-16-37-236.near.illinois.edu>
2025-01-17 12:00:47 -0600

    post group meeting

--- HyperspectralAnalysis.py
+++ HyperspectralAnalysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import curve_fit
 
 
 class HyperspectralAnalysis:
@@ -35,14 +36,20 @@ class HyperspectralAnalysis:
                 # properties = channel.properties
                 waves = channel[:]
                 break
-        return bot_pix, top_pix, waves, data_cube
+        pixel_params = [bot_pix, top_pix, ncol, nrow]
+        return pixel_params, waves, data_cube
 
-    def process_wcdc(self, dc_raw, bc_raw):
+    def process_wcdc(self, dc_raw, bc_raw, pixel_params):
         dc = np.mean(dc_raw, 0)
         bc = np.mean(bc_raw, 0)
         standard = bc - dc  # called stan in matlab
         stan = standard - np.min(standard) + 0.1  # don't think this matters
-        return stan
+        bot_pix, top_pix, ncol, nrow = pixel_params
+        stanim = np.zeros((ncol, nrow, 670))
+        for i in range(ncol):
+            stanim[i, :, :] = stan[top_pix - 1:bot_pix, ::2]
+
+        return stanim
 
     def calc_background_avg(self, I_raw, percent):
         bkavg = np.zeros((670,))
@@ -63,16 +70,94 @@ class HyperspectralAnalysis:
         # Find the average of all the pixels
         bkavg = sum(I_raw[idx_a_cut, idx_b_cut, :]) / numPixelsBack
         bkavg = np.round(bkavg, 5)  # This is to exactly match matlab
-        return bkavg
+        print(idx_a_cut.shape)
+        return bkavg, [idx_a_cut, idx_b_cut]
 
-    def calc_DFS_onepixel(self, inten_raw, stan, bkavg, top_pix, bot_pix):
-        stanim = np.zeros((103, 261, 670))
-        for i in range(103):
-            stanim[i, :, :] = stan[top_pix - 1:bot_pix, ::2]
-        specfin = np.round((inten_raw - bkavg) / stanim, 5)
+    def fit_2Dgauss(self, M, a, x0, c1, y0, c2, d):
+        x, y = M
+        return 1E3 * a * np.exp(-((x - x0)**2 / (2 * c1**2) + (y - y0)**2
+                                  / (2 * c2**2))) + d * 1E3
+
+    def fitNP_eachwave(self, specfin, waveidx, xi, yi):
+        # Find the background at a given wavelength
+        specfin_wind = specfin[xi - 10: xi + 10, yi - 10: yi + 10, waveidx]
+        xran = np.linspace(0, specfin_wind.shape[0] - 1, specfin_wind.shape[0])
+        yran = np.linspace(0, specfin_wind.shape[1] - 1, specfin_wind.shape[1])
+        X, Y = np.meshgrid(xran, yran)
+        xdata = np.vstack((X.ravel(), Y.ravel()))
+        Z = specfin_wind.ravel()
+        p0 = [1., 10., 0.86, 10., 1., 2.]
+        bounds = [[0, 0, 0, 0, 0, 0],
+                    [1000, 1000, 1000, 1000, 1000, 1000]]
+        popt, pcov = curve_fit(self.fit_2Dgauss, xdata, Z, p0, bounds=bounds)
+        fitdata = self.fit_2Dgauss(xdata, *popt).reshape(len(yran), len(xran))
+        return specfin_wind, fitdata, popt
+
+    def fit_all_NPs_eachwave(self,
+                             inten_raw,
+                             positions,
+                             background,
+                             numPart,
+                             numWaves,
+                             wavei,
+                             wavef):
+        # At some wavelengths, we cannot fit a background.
+        # In this case, the code just grabs the previously defined background.
+        # I'll silence the error messages that arise.
+        import warnings
+        warnings.simplefilter("ignore")
+
+        for npi in range(numPart):
+            xi = int(positions[npi, 0]) - 1
+            yi = int(positions[npi, 1]) - 1
+            for wi in range(numWaves):
+                try:
+                    _, _, popt = self.fitNP_eachwave(specfin=inten_raw,
+                                                     waveidx=(wi + wavei),
+                                                     xi=xi,
+                                                     yi=yi)
+                    # print(popt)
+                except RuntimeError:
+                    pass
+
+                background[npi, wi] = np.round(popt[-1] * 1E3, 1)
+        return background
+
+    def calc_DFS(self, inten_raw, wc_minus_dc, back):
+        specfin = np.round((inten_raw - back) / wc_minus_dc, 5)
         return specfin
 
-    def calc_DFS_avg3pixels(self, specfin, xi, yi):
-        sum_DFS = np.sum(specfin[xi - 1:xi + 2, yi - 1:yi + 2, :], axis=(0, 1))
-        return np.round(sum_DFS, 5)
+    def avg3pixels(self, M, xi, yi):
+        sum_M = np.sum(M[xi - 1:xi + 2, yi - 1:yi + 2, :], axis=(0, 1))
+        return np.round(sum_M, 5)
+
+    def calc_DFS_localback(self,
+                           inten_raw,
+                           wc_minus_dc,
+                           background,
+                           npi,
+                           xi,
+                           yi,
+                           wavei,
+                           wavef):
+        inten_norm_avg3 = self.avg3pixels(M=(inten_raw / wc_minus_dc),
+                                          xi=xi,
+                                          yi=yi,
+                                          )
+        # total = self.calc_DFS(inten_raw=inten_norm_avg3[wavei:wavef],
+        #                       wc_minus_dc=wc_minus_dc[xi, yi, wavei:wavef],
+        #                       back=(9 * background[npi, :]))
+        total = np.round(inten_norm_avg3[wavei:wavef]- 9*background[npi, :] / wc_minus_dc[xi, yi, wavei:wavef], 5)
+
+        return total
+
+    def lorentz(self, wave_eV, A, Gam_eV, wave0_eV):
+        return A * 0.5 * Gam_eV / ((wave_eV - wave0_eV)**2 + (0.5 * Gam_eV)**2)
+
+    def fit_spectrum(self, wave, inten):
+        popt, pcov = curve_fit(self.lorentz, wave, inten,
+                               p0=[100, 200, 770])
+        fit = self.lorentz(wave, *popt)
+        return fit, popt
+
 
--- README.md
+++ README.md
@@ -3,16 +3,19 @@
 ## Matlab codes
 The group of matlab codes does nothing functionally differently than the current hyperspectral processing codes in the Link / Landes Group. I have recreated the matlab codes in python. I verified that the code exactly reproduces matlab. To check yourself, follow along with the Jupyter Notebook, `Match Matlab code and Python`.
 
+
 ## How to run Matlab
 * First run `Hyper_Analysis.m`. This file does that data processing that turns the raw data into the correctly normalized data.
-** To do this, update line 8 `fold` to the folder that contains your raw data.
-** Create a file in that folder called `mydata.txt` that contains the white counts, dark counts, and data file.
-** A few files now should be greated in this folder.
+	* To do this, update line 8 `fold` to the folder that contains your raw data.
+	* Create a file in that folder called `mydata.txt` that contains the white counts, dark counts, and data file.
+	* A few files now should be greated in this folder.
 
 * Next, run `plot_particle_spectra_basic.m`.
-** Update lines 8-9 to be the folder that contains the raw data, and another folder that you'll make of the same name as the data file.
-** Make the above folder inside the data folder.
-** Make a file called positions.txt, you'll populate it shortly.
-** Run the first two blocks in matlab. One by one, identify the coordinates of each NP and place them in the positions text file.
-** Once this is done, run the rest of the script.
+	* Update lines 8-9 to be the folder that contains the raw data, and another folder that you'll make of the same name as the data file.
+	* Make the above folder inside the data folder.
+	* Make a file called positions.txt, you'll populate it shortly.
+	* Run the first two blocks in matlab. One by one, identify the coordinates of each NP and place them in the positions text file.
+	* Once this is done, run the rest of the script.
 
+
+## How to run Python
